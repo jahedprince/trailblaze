d6c0272e7035aa462216f8d6be9c88cf0303d5f8
jahedprince <jahedprince183@gmail.com>
2023-09-11 23:29:39 -0400

    edit and delete acitivites

--- screens/MainAppFlow/ItineraryDetailsScreen.js
+++ screens/MainAppFlow/ItineraryDetailsScreen.js
@@ -7,6 +7,8 @@ import {
   Image,
   FlatList,
   TouchableOpacity,
+  TextInput,
+  Button,
 } from "react-native";
 import { initializeApp } from "firebase/app";
 import {
@@ -16,31 +18,162 @@ import {
   orderBy,
   limit,
   onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import { LinearGradient } from "expo-linear-gradient";
-
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
 import { useSelector } from "react-redux";
 import BottomNavigation from "../../components/BottomNavigation";
+import Swipeout from "react-native-swipeout";
 
 const ItineraryDetailsScreen = ({ route }) => {
   const { itinerary } = route.params;
+  const [editingActivity, setEditingActivity] = useState(null);
+  const [itineraryData, setItineraryData] = useState(itinerary);
 
   if (!itinerary) {
     return <Text>Loading...</Text>;
   }
 
-  const renderDayItem = ({ item }) => {
-    const onPress = () => {
-      // Handle press event for a day item
-      console.log("Day item pressed:", item);
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: FIREBASE_API_KEY,
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      projectId: FIREBASE_PROJECT_ID,
+      storageBucket: FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+      appId: FIREBASE_APP_ID,
     };
 
-    const activities = item.activities.map((activity) =>
-      activity.startsWith("-") ? activity.slice(1).trim() : activity
-    );
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    // Add Firestore queries and listeners here
+    return () => {
+      // Cleanup code if needed
+    };
+  }, []);
+
+  const renderDayItem = ({ item, index }) => {
+    const onEdit = (activityIndex) => {
+      setEditingActivity({ dayIndex: index, activityIndex });
+    };
+
+    const onDelete = (activityIndex) => {
+      const updatedItinerary = { ...itineraryData };
+      updatedItinerary.days[index].activities.splice(activityIndex, 1);
+
+      // Update the local state to reflect the deleted activity
+      const updatedItineraryCopy = { ...itineraryData };
+      updatedItineraryCopy.days[index].activities =
+        updatedItinerary.days[index].activities;
+
+      setItineraryData(updatedItineraryCopy); // Update the state with the modified itinerary
+
+      const db = getFirestore();
+      const itineraryRef = doc(db, "itineraries", itinerary.id);
+
+      // Update Firestore with the updated activities array
+      updateDoc(itineraryRef, {
+        days: updatedItinerary.days,
+      });
+    };
+
+    const updateFirestoreWithActivity = (activityIndex, text) => {
+      const updatedItinerary = { ...itinerary };
+      updatedItinerary.days[index].activities[activityIndex] = text;
+
+      const db = getFirestore();
+      const itineraryRef = doc(db, "itineraries", itinerary.id);
+
+      // Update the Firestore document with the updated activity
+      updateDoc(itineraryRef, {
+        days: updatedItinerary.days,
+      });
+    };
+
+    const activities = item.activities.map((activity, activityIndex) => {
+      const isEditing =
+        activityIndex === editingActivity?.activityIndex &&
+        editingActivity?.dayIndex === index;
+
+      const onPress = () => {
+        if (!isEditing) {
+          setEditingActivity({
+            dayIndex: index,
+            activityIndex,
+          });
+        }
+      };
+
+      return (
+        <Swipeout
+          right={[
+            {
+              text: "Edit",
+              backgroundColor: "#357FEE",
+              onPress: () => onEdit(activityIndex),
+            },
+            {
+              text: "Delete",
+              backgroundColor: "#E92B2B",
+              onPress: () => onDelete(activityIndex), // Call the onDelete function here
+            },
+          ]}
+          autoClose={true}
+          backgroundColor="transparent"
+          key={activityIndex}
+        >
+          <TouchableOpacity onPress={onPress}>
+            {isEditing ? (
+              <TextInput
+                style={styles.itineraryDetails}
+                value={activity}
+                onChangeText={(text) => {
+                  // Update the local state
+                  const updatedItinerary = { ...itinerary };
+                  updatedItinerary.days[index].activities[activityIndex] = text;
+                  setEditingActivity({
+                    dayIndex: index,
+                    activityIndex,
+                  });
+                }}
+                onBlur={() => {
+                  setEditingActivity(null);
+                  // Update Firestore with the new activity text here
+                  updateFirestoreWithActivity(activityIndex, activity);
+                }}
+                onSubmitEditing={() => {
+                  setEditingActivity(null);
+                  // Update Firestore with the new activity text here
+                  updateFirestoreWithActivity(activityIndex, activity);
+                }}
+                autoFocus
+                multiline
+                blurOnSubmit={true}
+              />
+            ) : (
+              <Text style={styles.itineraryDetails}>
+                {activity.startsWith("-") ? activity.slice(1).trim() : activity}
+              </Text>
+            )}
+          </TouchableOpacity>
+          {activityIndex !== item.activities.length - 1 && <Text>{"\n"}</Text>}
+        </Swipeout>
+      );
+    });
 
     return (
-      <TouchableOpacity style={styles.itineraryItem} onPress={onPress}>
+      <View style={styles.itineraryItem}>
         <LinearGradient
           colors={["rgba(7, 54, 63, 1)", "rgba(10, 46, 53, 1)"]}
           style={styles.linearGradient}
@@ -48,15 +181,9 @@ const ItineraryDetailsScreen = ({ route }) => {
           <View style={styles.iconAndHeader}>
             <Text style={styles.header}>{`Day ${item.dayNumber}`}</Text>
           </View>
-          {item.activities.map((activity, activityIndex) => (
-            <Text style={styles.itineraryDetails} key={activityIndex}>{`${
-              activity.startsWith("-") ? activity.slice(1).trim() : activity
-            }${
-              activityIndex !== item.activities.length - 1 ? "\n" : ""
-            }`}</Text>
-          ))}
+          {activities}
         </LinearGradient>
-      </TouchableOpacity>
+      </View>
     );
   };
 
@@ -95,6 +222,9 @@ const ItineraryDetailsScreen = ({ route }) => {
 };
 
 const styles = StyleSheet.create({
+  activityContainer: {
+    marginBottom: 5,
+  },
   container: {
     flex: 1,
     backgroundColor: "black",
