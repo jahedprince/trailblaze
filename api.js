143c6b1603ecfe3ed557b61e605708b4e53dc527
jahedprince <jahedprince183@gmail.com>
2023-09-22 12:18:40 -0400

    logic changes

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const createTravelItinerary = async (destination, duration, apiKey) => {
     const response = await axios.post(
       "https://api.openai.com/v1/engines/text-davinci-003/completions",
       {
-        prompt: `give me a detailed travel itinerary with multiple activities for each day from morning, afternoon, evening and night to ${destination} for ${duration} days. can you make sure to have different activites throughout the duration of the trip. also, please make sure the activites during each day are reasonable/doable taking into account travel time from one acitvity to another, and hours in a day with having to sleep and rest. please add how many hours each activity should take at the end of each activity like (__ hrs). If one of the days are day trips, please take into account traveling to the day trip place`,
+        prompt: `give me a detailed travel itinerary with multiple activities for each day from morning, afternoon, evening and night to ${destination} for ${duration} days. can you make sure to have different activites throughout the duration of the trip. also, please make sure the activites during each day are reasonable/doable taking into account travel time from one acitvity to another, and hours in a day with having to sleep and rest. If one of the days include a day trip, please take into account traveling to the day trip place`,
         max_tokens: 1200,
         temperature: 0.2,
         n: 1,
